6819c5a7f2272097a972f8b81252e6477c1d4cb0
Luiz Kim <luiz.kim@controleonline.com>
2024-03-24 21:19:56 -0300

    People

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace ControleOnline\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
