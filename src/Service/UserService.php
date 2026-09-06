e789bd97f6087dff3247589d75c689bdb2b9d3e5
Luiz Kim <luiz.kim@controleonline.com>
2026-09-06 12:34:13 -0300

    test: document api-platform-users people smoke flow

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -1,4 +1,5 @@
 <?php
+// fluxo: cliente-cadastro, funcionario-cadastro, fornecedor-cadastro, minhas-empresas-cadastro, franquia-cadastro, vendedor-cadastro, motoboy-cadastro, transportador-cadastro | etapa: user-last-step | wiki: https://github.com/ControleOnline/api-community/wiki/Venda-Producao
 
 /*
  * Contract imported from AGENTS.md
